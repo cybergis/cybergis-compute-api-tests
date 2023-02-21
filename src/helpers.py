391f55fff2198291e043fc7ff8227a1f8ed3944f
jtsiv1 <johnspeaksiv@gmail.com>
2023-02-21 16:10:17 -0600

    updated tests and added new ones for all API routes

--- src/helpers.py
+++ src/helpers.py
@@ -13,7 +13,6 @@ def get_user_jobs():
     """
     return json.loads(config.get("/user/job").content)
 
-
 def get_user_job():
     """Gets the first job from the job data"""
     return get_user_jobs()["job"][0]
@@ -22,3 +21,57 @@ def get_user_job():
 def get_user_jobid():
     """Gets the job id for a user job"""
     return get_user_job()["id"]
+
+def get_user_folderid():
+    """Gets the the ID of a user's folder"""
+    return get_user_job()['remoteExecutableFolder']['id']
+
+def confirm_job_format(job):
+    to_return = 'id' in job
+    to_return = to_return and 'userId' in job
+    to_return = to_return and 'secretToken' in job
+    to_return = to_return and 'slurmId' in job
+    to_return = to_return and 'maintainer' in job
+    to_return = to_return and 'hpc' in job
+    to_return = to_return and 'remoteExecutableFolder' in job
+    to_return = to_return and 'remoteDataFolder' in job
+    to_return = to_return and 'remoteResultFolder' in job
+    to_return = to_return and 'localExecutableFolder' in job
+    to_return = to_return and 'localDataFolder' in job
+    to_return = to_return and 'param' in job
+    to_return = to_return and 'env' in job
+    to_return = to_return and 'slurm' in job
+    to_return = to_return and 'createdAt' in job
+    to_return = to_return and 'updatedAt' in job
+    to_return = to_return and 'deletedAt' in job
+    to_return = to_return and 'initializedAt' in job
+    to_return = to_return and 'finishedAt' in job
+    to_return = to_return and 'isFailed' in job
+    to_return = to_return and 'events' in job
+    to_return = to_return and 'logs' in job
+    return to_return
+
+def confirm_logs_format(logs):
+    to_return = 'id' in logs
+    to_return = to_return and 'jobId' in logs
+    to_return = to_return and 'message' in logs
+    to_return = to_return and 'createdAt' in logs
+    to_return = to_return and 'updatedAt' in logs
+    to_return = to_return and 'deletedAt' in logs
+    return to_return
+
+def confirm_job_event_format(event):
+    return confirm_logs_format(event) and 'type' in event
+
+def confirm_valid_folder(folder):
+    to_return = 'id' in folder
+    to_return = to_return and 'name' in folder
+    to_return = to_return and 'hpc' in folder
+    to_return = to_return and 'hpcPath' in folder
+    to_return = to_return and 'globusPath' in folder
+    to_return = to_return and 'userId' in folder
+    to_return = to_return and 'isWritable' in folder
+    to_return = to_return and 'createdAt' in folder
+    to_return = to_return and 'updatedAt' in folder
+    to_return = to_return and 'deletedAt' in folder
+    return to_return
--- src/test/public_routes.py
+++ src/test/public_routes.py
@@ -1,11 +1,13 @@
 """
 This file is for testing that the API is up and public routes (no authentication)
 
+    "/",
     "/container",
     "/git",
     "/hpc",
     "/maintainer",
-    "/statistic"
+    "/statistic",
+    "/whitelist"
 """
 import json
 import pytest
@@ -14,30 +16,26 @@ sys.path.insert(0, "./src")
 import config  # noqa
 
 
+"""
+Testing (GET) for /, /container, /git, /hpc, /maintainer, /statistic, and /whitelist
+"""
 @pytest.mark.parametrize("test_input", [
     (""),
     ("/container"),
     ("/git"),
     ("/hpc"),
     ("/maintainer"),
-    ("/statistic")
+    ("/statistic"),
+    ("/whitelist")
 ])
-def test_can_access_page(test_input):
-    """Tests that I can get the base url"""
+def test_can_access_page_and_load_response(test_input):
+    """Tests that base url is accessible and that the content for each path can be loaded with JSON with or without a token"""
     response = config.get(test_input, auth=False)
-    assert response.status_code == 200
-
-
-@pytest.mark.parametrize("test_input", [
-    (""),
-    ("/container"),
-    ("/git"),
-    ("/hpc"),
-    ("/maintainer"),
-    ("/statistic")
-])
-def test_can_load_json_response(test_input):
-    """Tests that the content for /container can be loaded with JSON"""
-    response = config.get(test_input, auth=False)
+    response2 = config.get(test_input)
+    assert response.content == response2.content # same response with and without auth
+    assert response.status_code == 200 
+    assert response2.status_code == 200 # successful requests
     content = response.content
-    json.loads(content)
+    content2 = response2.content
+    assert json.loads(content) is not None
+    assert json.loads(content2) is not None # something is recieved for data
